79e461f3af9f47a540b8ea848e4b66cb77d53b4f
Xiaoleng <zzw510@foxmail.com>
2024-06-20 17:22:50 +0800

    Update colorful.js

--- js/colorful.js
+++ js/colorful.js
@@ -421,12 +421,12 @@ const htmlConfig = {
     <p>这里是每天都在追逐星光梦想的你最可爱的小冷<br>
     I am your cutest Xiaoleng, chasing the dreams of starlight every day.</p>
     <p>与小冷一起怀着对舞台的憧憬和闪闪发光✨的梦想<br>
-    Join Xiaoleng as we embrace the allure of the stage and our sparkling dreams✨<br>
-    让我们沉溺于这场药物💊甜腻的梦中<br>
-    Let's indulge in this sweet, 💊drug-filled dream.</p>
-    <p>也请来了解你最可爱的小偶像小冷<br>
-    请多多关注小冷吧~<br>
-    Also, come get to know Xiaoleng, your cutest idol.<br>
+    Join Xiaoleng as we embrace the allure of the stage and our sparkling dreams✨</p>
+    <p>让我们沉溺于这场药物💊甜腻的梦中<br>
+    Let's indulge in this sweet, 💊drug-filled dream.<br>
+    也请来了解你最可爱的小偶像小冷<br>
+    Also, come get to know Xiaoleng, your cutest idol.</p>
+    <p>请多多关注小冷吧~<br>
     Please pay more attention to Xiaoleng~</p>`,
 }
 const urlConfig = {'pasta.html': 'pasta.html'};
