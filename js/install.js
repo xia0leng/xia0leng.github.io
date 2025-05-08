2f2cf6f79b96e69c7d8a88f80527586d20964359
Xiaoleng <lqmeow@foxmail.com>
2025-05-08 20:14:59 +0800

    1

--- js/install.js
+++ js/install.js
@@ -1,1 +1,10 @@
-<button id="btn" style="display:none" onclick="pwaEvent.prompt()">添加</button>
+document.getElementById('install-btn').addEventListener('click', async () => {
+  if (window.pwaEvent) {
+    window.pwaEvent.prompt();
+    const result = await window.pwaEvent.userChoice;
+    console.log(result.outcome);
+    window.pwaEvent = null;
+  } else {
+    alert("浏览器尚未准备好安装提示喵～");
+  }
+});
