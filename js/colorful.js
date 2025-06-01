f5858212ef88513a2bb0180fa448c01103ad047e
Xiaoleng <lqmeow@foxmail.com>
2025-06-02 07:42:27 +0800

    Update colorful.js

--- js/colorful.js
+++ js/colorful.js
@@ -82,6 +82,7 @@ var config = {
 				icon:'img',
                 value: 'https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fxlnww%2Fposts%2Fpfbid02G4hvf3fSvMZsMFvqNhRzmkKxTLDs6Ycz8zrZja2m3njUvJf8whAyc5oMfkABcGh4l&show_text=true',
                 style: ['large']
+			},
 			
         },
         desc: '这里汇聚了小冷的所有闪耀瞬间：舞台闪光、日常萌拍、偶尔流露的病娇小表情<br> All my shining moments—stage highlights, everyday cuteness, and the occasional yandere glance.<br> ⚠️照片iframe来源是Facebook，在墙内打不开是正常的<br> ⚠️Content is embedded from Facebook; use a VPN or proxy if it doesn&#39;t load.',
