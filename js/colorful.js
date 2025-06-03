295270884af95fcee90a4ab79bfc44cfc6c756a9
Xiaoleng <lqmeow@foxmail.com>
2025-06-04 01:10:58 +0800

    Update colorful.js

--- js/colorful.js
+++ js/colorful.js
@@ -63,7 +63,7 @@ var config = {
                 style: ['large']
             },
         },
-        desc: '这里汇聚了小冷的所有闪耀瞬间：舞台闪光、日常萌拍、偶尔流露的病娇小表情<br> All my shining moments—stage highlights, everyday cuteness, and the occasional yandere glance.<br> ⚠️照片iframe来源是Facebook，在墙内打不开是正常的<br> ⚠️Content is embedded from Facebook; use a VPN or proxy if it doesn&#39;t load.',
+        desc: '这里汇聚了小冷的所有闪耀瞬间：舞台闪光、日常萌拍、偶尔流露的病娇小表情<br> All my shining moments—stage highlights, everyday cuteness, and the occasional yandere glance.',
         style: ['medium'],
         link: ['entry', 'icon']
     },
@@ -94,7 +94,7 @@ var config = {
                 style: ['large']
             },
         },
-        desc: '这里收藏了小冷在舞台上最闪亮的瞬间<br> Here my brightest magical moments on stage.<br> ⚠️视频iframe来源是Facebook，在墙内打不开是正常的<br> ⚠️Content is embedded from Facebook; use a VPN or proxy if it doesn&#39;t load.',
+        desc: '这里收藏了小冷在舞台上最闪亮的瞬间<br> Here my brightest magical moments on stage.',
         style: ['medium'],
         link: ['entry', 'icon']
     },
