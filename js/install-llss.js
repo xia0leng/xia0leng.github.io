34c799ca24098bc22fc83b10d84294c7fbe6f330
Xiaoleng <lqmeow@foxmail.com>
2025-06-28 03:43:41 +0800

    1

---
+++ js/install-llss.js
@@ -0,0 +1,23 @@
+window.root.innerHTML = `
+  <div style="padding: 20px; font-size: 16px;">
+    <p>可以将L²S²安装为 App<br>
+	   You can install L²S² as an App
+	</p>
+    <button id="install-btn" style="margin-top:10px;">点击安装 Click to Install</button>
+  </div>
+`;
+
+document.getElementById('install-btn').addEventListener('click', async () => {
+  if (window.pwaEvent) {
+    window.pwaEvent.prompt();
+    const result = await window.pwaEvent.userChoice;
+    if (result.outcome === 'accepted') {
+      window.root.innerHTML += `<p style="color:green;margin-top:10px;">安装成功 Successfully installed 🎉</p>`;
+    } else {
+      window.root.innerHTML += `<p style="color:red;margin-top:10px;">已取消安装 Installation cancelled</p>`;
+    }
+    window.pwaEvent = null;
+  } else {
+    alert("你使用了不支持的浏览器，或处于隐身模式\nYou are using an unsupported browser or are in incognito mode.\n\niOS用户请使用Safari，并点击 Safari 底部的“分享”按钮，然后选择“添加到主屏幕”来安装此应用\nFor iOS users, please use Safari. Tap the \"Share\" button at the bottom, then select \"Add to Home Screen\" to install this app.");
+  }
+});
