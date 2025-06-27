9620413d8332484e80e3575907728eedcf981288
Xiaoleng <lqmeow@foxmail.com>
2025-06-28 04:02:03 +0800

    Update install-odw.js

--- js/install-odw.js
+++ js/install-odw.js
@@ -1,9 +1,8 @@
 window.root.innerHTML = `
   <div style="padding: 20px; font-size: 16px;">
-    <p>可以将OverdoseWiki安装为 App<br>
-	   You can install OverdoseWiki as an App
+    <p>可以将od-wiki安装为 App<
 	</p>
-    <button id="install-btn" style="margin-top:10px;">点击安装 Click to Install</button>
+    <button id="install-btn" style="margin-top:10px;">点击安装</button>
   </div>
 `;
 
@@ -12,12 +11,12 @@ document.getElementById('install-btn').addEventListener('click', async () => {
     window.pwaEvent.prompt();
     const result = await window.pwaEvent.userChoice;
     if (result.outcome === 'accepted') {
-      window.root.innerHTML += `<p style="color:green;margin-top:10px;">安装成功 Successfully installed 🎉</p>`;
+      window.root.innerHTML += `<p style="color:green;margin-top:10px;">安装成功 🎉</p>`;
     } else {
-      window.root.innerHTML += `<p style="color:red;margin-top:10px;">已取消安装 Installation cancelled</p>`;
+      window.root.innerHTML += `<p style="color:red;margin-top:10px;">已取消安装</p>`;
     }
     window.pwaEvent = null;
   } else {
-    alert("你使用了不支持的浏览器，或处于隐身模式\nYou are using an unsupported browser or are in incognito mode.\n\niOS用户请使用Safari，并点击 Safari 底部的“分享”按钮，然后选择“添加到主屏幕”来安装此应用\nFor iOS users, please use Safari. Tap the \"Share\" button at the bottom, then select \"Add to Home Screen\" to install this app.");
+    alert("你使用了不支持的浏览器，或处于隐身模式\n\niOS用户请使用Safari，并点击 Safari 底部的“分享”按钮，然后选择“添加到主屏幕”来安装此应用");
   }
 });
